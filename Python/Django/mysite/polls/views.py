286873fc356616e957bcf2417c2bed5d0d618eb0
kofujiBlank <baddream.sugar@gmail.com>
2019-06-09 01:13:52 +0900

    Django DAY3

--- Python/Django/mysite/polls/views.py
+++ Python/Django/mysite/polls/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 # from django.template import loader
 from .models import Question
+from django.shortcuts import get_object_or_404
 
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -12,10 +12,18 @@ def index(request):
 	context = {'latest_question_list':latest_question_list}
 	# return HttpResponse(template.render(context, request))
 	return render(request, 'polls/index.html', context)
+
 def detail(request,question_id):
-	return HttpResponse("You're looking at Question %s." % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	# try:
+	#	question = Question.objects.get(pk=question_id)
+	# except Question.DoesNotExist:
+	#	raise Http484("Question does not exist")
+	return render(request, 'polls/detail.html', {'question':question})
+
 def results(request, question_id):
 	response = "You're looking at the results of question %s."
 	return HttpResponse(resoinse % question_id)
+
 def vote(request, question_id):
 	return HttpResponse("You're voting on question %s." % question_id)

7cd72ab7c9c542c4255b65e22a2d250f3b0f7621
kofujiBlank <baddream.sugar@gmail.com>
2019-06-08 22:43:33 +0900

    Django basic 更新

--- Python/Django/mysite/polls/views.py
+++ Python/Django/mysite/polls/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# from django.template import loader
+from .models import Question
+
 def index(request):
-	return HttpResponse("HelloWorld You're at the polls index.")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	# DAY3-3/7
+	# output = ', '.join([q.question_text for q in latest_question_list])
+	# template = loader.get_template('polls/index.html')
+	context = {'latest_question_list':latest_question_list}
+	# return HttpResponse(template.render(context, request))
+	return render(request, 'polls/index.html', context)
+def detail(request,question_id):
+	return HttpResponse("You're looking at Question %s." % question_id)
+def results(request, question_id):
+	response = "You're looking at the results of question %s."
+	return HttpResponse(resoinse % question_id)
+def vote(request, question_id):
+	return HttpResponse("You're voting on question %s." % question_id)
